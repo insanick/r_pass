3f91f3b355842f0cc4668cb802453459866e9df4
insanick <salocin.reis@gmail.com>
2025-12-28 10:33:33 -0500

    Reworked quick_build to use safer symbols

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,7 @@
 //! 
 //! `./r_pass`
 
-
-use std::io::{self, Write};
-use rand::{Rng,prelude::IndexedRandom};
-use sim_put::Input;
+use rand::prelude::IndexedRandom;
 /// Quickly creates a random string of characters for use
 /// 
 /// # Example
@@ -30,12 +27,9 @@ use sim_put::Input;
 /// This will be addresses gracefully in a future release 
 /// 
 pub fn quick_build() {
-    // Quickly creates a random string
-
-    // Create an array containing the keys we will use
     let keyspace = ['!','#','$','%','&','(',')','+','-','0','1','2','3','4','5','6','7','8','9',':',';','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     
-    // Initialize the Rng, the password string, and the current_char placeholder
+
     let mut rng = rand::rng();
     let mut password: String = String::new();
     let mut current_char: &char;
@@ -50,75 +44,7 @@ pub fn quick_build() {
 
 /// Shows the usage prompt if required.
 pub fn show_help() {
-    println!("Usage: ./r_pass [options]\n");
-    println!("\n[r_pass] will help create a password based on prompts or options[future-feature].");
-    println!("When used without flags or prompts, creates a quick secure* secret key.");
+    println!("Usage: ./r_pass\n");
+    println!("Creates a quick secure* secret key.");
     println!("*[16 character string, alphanumeric and symbols]")
-}
-
-
-#[derive(Debug)]
-enum Complexity {
-    Simple,
-    Pin,
-    Complex,
-}
-
-#[derive(Debug)]
-pub struct Password {
-    password: Vec<char>,
-    length: u8,
-    complexity: Complexity
-}
-
-/// Constructor for the Password struct. 
-/// 
-/// Calls private functions that will prompt the user
-impl Password {
-    pub fn build() -> Password {
-        Password {
-            password: Vec::new(),
-            length: Password::get_length(),
-            complexity: Password::get_complexity()
-        }
-    }
-
-    fn get_length() -> u8 {
-        
-        let new_length = Input::prompted_input("Password length");
-
-        let new_length: u8 = new_length
-            .trim()
-            .parse()
-            .expect("Not a number!");
-
-        new_length
-    }
-
-    fn get_complexity() -> Complexity {
-        Password::complexity_prompt();
-
-        let choice = Input::prompted_input("Complexity");
-
-
-
-        let choice: u8 = choice.trim().parse().expect("not a digit");
-
-        match choice {
-            1 => Complexity::Simple,
-            2 => Complexity::Complex,
-            3 => Complexity::Pin,
-            _ => {
-                println!("Not an option!");
-                Password::get_complexity()
-            }
-        }
-    }
-
-    fn complexity_prompt() {
-        println!("1) Simple (Chars and Nums)");
-        println!("2) Complex (Adds Symbols)");
-        println!("3) Pin Mode (Digits only)");
-        }
-
 }
